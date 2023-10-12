e257bd449ecc270eb599d3245728608b86b512a1
Amay Kataria <amay.kataria@gmail.com>
2023-10-12 17:59:17 -0500

    Tailwind has been enabled and the CSS is much more cleaned up. A select button has been added and all the Execution IDs have been mapped.

--- Client/src/components/CoverPage.tsx
+++ Client/src/components/CoverPage.tsx
@@ -3,85 +3,10 @@
     Date: 09/17/2023
     Description: Cover page for the MVP
 */
-import axios from 'axios';
 import { useState } from "react"
 import useGlobalStore from "./stores/useGlobalStore";
 import DuneController from "./controllers/DuneController";
 
-const styles = {
-    artists: {
-        marginTop: '10px',
-        fontSize: '16px',
-        fontStyle: 'italic'
-    },
-
-    github: {
-        marginTop: '8px',
-        fontSize: '18px'
-    },
-
-    title: {
-        marginTop: '220px',
-        fontSize: '120px',
-        fontWeight: '8',
-        marginBottom: '20px',
-        fontFamily: 'Brush Script MT'
-    },
-
-    subtitle: {
-        fontSize: '30px',
-        fontWeight: 'bold',
-        marginBottom: '50px',
-        fontFamily: 'monospace',
-        textDecoration: 'underline'
-    },
-
-    titleSmall: {
-        fontSize: '30px',
-        fontWeight: '8',
-        marginBottom: '100px',
-        fontFamily: 'Brush Script MT'
-    },
-
-    input: {
-        borderStyle: 'solid',
-        borderColor: 'blue ',
-        height: '50px',
-        width: '350px',
-        fontSize: '15px',
-        fontColor: 'green',
-        fontWeight: 'bold'
-    },
-
-    button: {
-        marginLeft: '20px',
-        borderStyle: 'solid',
-        borderColor: 'blue',
-        fontSize: '20px',
-        height: '50px'
-    },
-
-    container: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-    },
-
-    actionContainer: {
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center'
-    },
-
-    infoContainer: {
-        display: 'flex',
-        flexDirection: 'column',
-        marginBottom: '20px'
-    }
-}
-
-const defaultEthAddress = "0x5af0d9827e0c53e4799bb226655a1de152a425a5";
-const defaultNumAgents = 200;
 
 function getUniqueHolders(data: any) {
     // Extract all holders from the array
@@ -94,8 +19,8 @@ function getUniqueHolders(data: any) {
 }
   
 export default function CoverPage (props) {
-    const [address, setAddress] = useState(defaultEthAddress);
-    const [numAgents, setNumAgents] = useState(defaultNumAgents);
+    // Default value of the select box is Azuki - this is the value associated with it.
+    const [value, setValue] = useState("01HCG9DM9TPF7KFPRV43W10RSY");
     const [hideUI, setHideUI] = useState(false);
 
     const handleChange = (event) => {
@@ -109,55 +34,52 @@ export default function CoverPage (props) {
     }
 
     const startContractExperience = (event) => {
-        setHideUI(true);
-        DuneController.getData('01HCG9P9N84K1ZJW35SXGDVP3W');
+        console.log("Current Value: " + value);
+        // setHideUI(true);
+
+        // Get this execute ID from the UI. 
+        // DuneController.getData('01HCG9P9N84K1ZJW35SXGDVP3W');
 
         // Wait a tiny bit before loading this jazz.
         // setTimeout(props.onShowExperience, 2000);
     }
 
-    const startRandomExperience = (event) => {
-        // Set a number of agents here.
-        console.log(numAgents);
-        useGlobalStore.setState({ numRandomAgents: numAgents });
-
-        setHideUI(true);
-        
-        // Wait a tiny bit before loading this jazz.
-        setTimeout(props.onShowExperience, 1000);
-    }
-
-    const uiBlock = () => {
-        return <>
-            <div style={styles.actionContainer}>
-                <div style={styles.infoContainer}>
-                    <div>Contract Address</div>
-                    <input onChange={handleChange} value={address} type="text" autoFocus style={styles.input} />
-                </div>
-                <button onClick={startContractExperience} style={styles.button}>GO</button>
-            </div>
-            <div style={styles.infoContainer}>
-                <div>Squiggles (Art Blocks): 0x8d137e3337eb1b58a222fef2b2cc7c423903d9cf</div>
-                <div>Fidenza (Art Blocks): 0x175eaf4feb0a147b5a77549389392094bf38b198</div>
-                <div>Board Apes: 0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d</div>
-            </div>
-            <div style={styles.actionContainer}>
-                <div style={styles.infoContainer}>
-                    <div>Random Whale Count</div>
-                    <input onChange={handleChangeNumber} value={numAgents} type="number" autoFocus style={styles.input} />
-                </div>
-                <button style={styles.button} onClick={startRandomExperience}>GO</button>
-            </div>
-        </>
+    const onItemSelected = (event) => {
+        setValue(event.target.value);
     }
 
     return <>
-        <div style={styles.container}>   
-            <div style={styles.title}>Whale Watcher</div>
-            <div style={styles.subtitle}>ETHChicago2023</div>
-            {hideUI ? <div style={styles.titleSmall}>Loading things up...</div> : uiBlock() }           
-            <div style={styles.artists}>Amay Kataria / Max Knivets / Blair McKee</div>
-            <a href={'https://github.com/eulphean/visualizer-hackathon-app'} target="_blank" style={styles.github}>Github</a>
+        <div className={'h-screen flex flex-col items-center justify-center'}>   
+            <div className="text-6xl font-serif italic">Whale Watcher</div>  
+            <div className="flex flex-col items-center gap-2 mt-6">
+                <div className="text-md font-semibold">
+                    NFT COLLECTION
+                </div>
+                <select className="w-40 h-10 border-gray-400 rounded shadow border-2 text-center"
+                    onChange={onItemSelected}>
+                    <option value="01HCG9DM9TPF7KFPRV43W10RSY">Azuki</option>
+                    <option value="01HCG9VGFPXMS5FX9M7420Q2MJ">DeGod</option>
+                    <option value="01HCG9MANW29W590JR4W5SHN63">Milady</option>
+                    <option value="01HCG8FS60VK0DRW28324K7JC2">Doodles</option>
+                    <option value="01HCG9GABVSKYCWQ9YBFTST2XQ">Moonbirds</option>
+                    <option value="01HCG9XV3H52FNJRY18QK034V7">The Potatoz</option>
+                    <option value="01HCG9W8QE5VZK5PE1ZEQSBAZF">The Captainz</option>
+                    <option value="01HCG9P9N84K1ZJW35SXGDVP3WR">Pudgy Penguins</option>
+                    <option value="01HCG9J9D9DNGKY2TW6NHG81AR">Wrapped CryptoPunks</option>
+                    <option value="01HCG9AF9S4E5ZDDQ2W3AJ2VAR">Bored Ape Yacht Club</option>
+                    <option value="01HCG9KCFQ6G1ZB6ZCJYNBGGFE">Mutant Ape Yacht Club</option>
+                </select> 
+                <button onClick={startContractExperience} className="bg-white hover:bg-gray-100 text-gray-800 font-semibold mt-1 py-1 px-4 border border-gray-400 rounded shadow">
+                    GO
+                </button>
+            </div>  
+            <div className="mt-3 flex flex-col items-center text-center">
+                <div>Amay Kataria / Max Knivets / Blair McKee</div>    
+                <div>Developed at ETHChicago 2023</div>
+            </div>
         </div>
     </>
 }
+
+// {hideUI ? <div>Loading things up...</div> : uiBlock() }           
+// <div>Amay Kataria / Max Knivets / Blair McKee</div>
